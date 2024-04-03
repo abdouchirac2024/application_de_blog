bffc5149c9acc0b03477a6a17d3807b908f82f72
abdouchirac2024 <abdouchirac411@gmail.com>
2024-04-03 17:43:24 +0200

    login

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import{BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home'
+import About from './pages/About'
+import SigIn from './pages/SigIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Footer from './components/Footer'
+
+import Header from './components/Header'
+
+
+export default function App() {
+  return (
+    <BrowserRouter >
+
+    <Header />
+    
+    <Routes>
+      
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/sigin-in' element={<SigIn />} />
+      <Route path='/sigin-up' element={<SignUp />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/projects' element={<Projects  />} />
+    </Routes>
+    <Footer />
+    </BrowserRouter>
+  )
+}
